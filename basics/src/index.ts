7674834ada979100b69212102e2748f1f44fa165
Rohith Appala <rohith@ziply.ai>
2026-06-27 18:16:40 +0530

    refactor: remove unused ts-node dependency and enhance type annotations in index.ts

--- basics/src/index.ts
+++ basics/src/index.ts
@@ -1,17 +1,18 @@
 import "dotenv/config";
-import { GoogleGenAI } from "@google/genai";
+import { GenerateContentResponse, GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY!,
 });
 
 async function main(): Promise<void> {
-  const response = await ai.models.generateContent({
+  const response: GenerateContentResponse = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: "Explain Node.js in one paragraph",
   });
 
-  console.log(response.text);
+  console.log("text response:", response.text);
+  console.log(response);
 }
 
 main().catch(console.error);
